9eb39d72f08470529c646bd7dce0ee63c6fef75d
Arthur Chagirov <multidevelop.java@gmail.com>
2023-08-20 12:18:24 +0300

    a sidebar complete

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -13,8 +13,8 @@ const Sidebar = () =>  (
             color: '#fff',
          }}
          key={category.name}>
-            <span> {category.icon}</span>
-            <span> {category.name}</span>
+            <span style={{color: category.name === selectedCategory ? 'white' : 'red', marginRight: '15px'}}> {category.icon}</span>
+            <span style={{opacity: category.name === selectedCategory ? '1' : '0.8' }}> {category.name}</span>
 
          </button>))}
 
