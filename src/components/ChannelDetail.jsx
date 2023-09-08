35df21469ddc3f578a65e9576756814ff8eb584a
Arthur Chagirov <multidevelop.java@gmail.com>
2023-09-08 13:49:20 +0300

    a new comment on a public rep

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+import { Box } from "@mui/material";
+
+import { Videos, ChannelCard } from "./";
 
 const ChannelDetail = () => {
-  return (
-    <div>ChannelDetail</div>
-  )
-}
+  return <div>ChannelDetail</div>;
+};
 
-export default ChannelDetail
+export default ChannelDetail;
