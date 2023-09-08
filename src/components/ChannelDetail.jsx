6d54a7da40afd26cca5a0244550d437fe0ca7e31
Arthur Chagirov <multidevelop.java@gmail.com>
2023-09-08 14:19:41 +0300

    1-41-20

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -4,8 +4,23 @@ import { useParams } from "react-router-dom";
 import { Box } from "@mui/material";
 
 import { Videos, ChannelCard } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const ChannelDetail = () => {
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`).then((data) =>
+      setChannelDetail(data?.items[0])
+    );
+
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then(
+      (data) => setVideos(data?.items)
+    );
+  }, [id]);
   return <div>ChannelDetail</div>;
 };
 
